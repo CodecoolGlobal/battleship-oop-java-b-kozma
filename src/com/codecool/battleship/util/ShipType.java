38525814d7508e094bf68ccccdad26818d095636
predavies <iamosaretin@gmail.com>
2021-10-04 14:05:04 +0200

    feat: add ship type enum

--- src/com/codecool/battleship/util/ShipType.java
+++ src/com/codecool/battleship/util/ShipType.java
@@ -1,4 +1,17 @@
 package com.codecool.battleship.util;
 
 public enum ShipType {
+    CARRIER(1),
+    CRUISER(2),
+    BATTLESHIP(3),
+    SUBMARINE(4),
+    DESTROYER(5);
+    private int length;
+
+    ShipType(int length) {
+        this.length = length;
+    }
+
+    public int getLength() {return length;}
+
 }
