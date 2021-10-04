eae43850cbcee19af02bf18bc779b663c7d7d7e5
b-kozma <kozma.balint.1997@gmail.com>
2021-10-04 19:52:27 +0200

    feat/ask for board size

--- src/com.codecool.battleship/Battleship.java
+++ src/com.codecool.battleship/Battleship.java
@@ -4,6 +4,7 @@ import com.codecool.battleship.board.Square;
 
 import java.util.Arrays;
 import java.util.List;
+import java.util.Scanner;
 import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
@@ -11,15 +12,28 @@ public class Battleship {
     public static void main(String [] args) {
 
         // Instantiate a Game
-        Game game = new Game();
+        int size = takeInputInteger("Give a table size between 10 and 20!");
+        Game game = new Game(size);
+
 
         // Test Assets:
-
         /// Print Square [][] board;
         Arrays.stream(game.getBoard().getSquareList()).forEach(row ->
         {Arrays.stream(row).forEach(square -> System.out.print(square.toString()));
             System.out.println("\n");
         });
 
+    }
+
+    public static String takeInputString(String message) {
+        System.out.println(message + "\n");
+        Scanner takeIn = new Scanner(System.in);
+        return takeIn.nextLine();
+    }
+
+    public static int takeInputInteger(String message) {
+        System.out.println(message + "\n");
+        Scanner takeIn = new Scanner(System.in);
+        return takeIn.nextInt();
     }
 }
--- src/com.codecool.battleship/Game.java
+++ src/com.codecool.battleship/Game.java
@@ -2,12 +2,34 @@ package com.codecool.battleship;
 
 import com.codecool.battleship.board.Board;
 
+import java.awt.*;
+import java.util.Scanner;
+
 public class Game {
     Board board;
 
-    public Game() {
-        this.board = new Board(10);
+    public Game(int size) {
+        if (isBetween(size, 10, 20)) {
+            this.board = new Board(size);
+        } else {
+            this.board = new Board(10);
+            System.out.println("Wrong input! Board is built with default parameters.");
+        }
     }
 
     public Board getBoard() {return board;}
+
+
+    public boolean isValid() {
+        return false;
+    }
+
+    public boolean isOutOfBounds(Point inputPoint) {
+        return false;
+    }
+
+    public boolean isBetween(int input, int min, int max) {
+        return (min <= input) && (input <= max);
+    }
+
 }
