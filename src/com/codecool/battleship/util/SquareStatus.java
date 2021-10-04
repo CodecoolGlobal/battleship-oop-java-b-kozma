f8de4c882a0ae3753c66df90a97b7c0613238a7a
predavies <iamosaretin@gmail.com>
2021-10-04 14:15:21 +0200

    feat: add square status

--- src/com/codecool/battleship/util/SquareStatus.java
+++ src/com/codecool/battleship/util/SquareStatus.java
@@ -1,4 +1,17 @@
 package com.codecool.battleship.util;
 
 public enum SquareStatus {
+    /*TODO: change this to uniCode characters*/
+
+    EMPTY(" "),
+    SHIP("S"),
+    HIT("H"),
+    MISSED("M");
+
+    private String uniCodeCh;
+    SquareStatus(String uniCodeCh) {
+        this.uniCodeCh = uniCodeCh;
+    }
+
+    public String getCharacter() {return uniCodeCh;}
 }
