9aec8eb9e6591686a72ba07dcaa9311ff1601a5e
predavies <iamosaretin@gmail.com>
2021-10-04 14:20:42 +0200

    feat: add square

--- src/com/codecool/battleship/board/Square.java
+++ src/com/codecool/battleship/board/Square.java
@@ -1,4 +1,17 @@
 package com.codecool.battleship.board;
 
-public class Square {
+import com.codecool.battleship.util.SquareStatus;
+
+import java.awt.Point;
+
+public class Square extends Point {
+    private SquareStatus status;
+
+    public Square(int x, int y) {
+        super(x, y);
+        this.status = SquareStatus.EMPTY;
+    }
+
+
+
 }
