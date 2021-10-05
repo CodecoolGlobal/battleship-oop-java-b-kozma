5721618389a46c57bb74722c7a55a45a3e33dd44
predavies <iamosaretin@gmail.com>
2021-10-05 08:01:28 +0200

    feat: [Battleship] add display and input fields

--- src/com.codecool.battleship/Battleship.java
+++ src/com.codecool.battleship/Battleship.java
@@ -1,5 +1,6 @@
 package com.codecool.battleship;
 
+import com.codecool.battleship.board.Display;
 import com.codecool.battleship.board.Square;
 
 import java.util.Arrays;
@@ -8,6 +9,10 @@ import java.util.stream.Collectors;
 import java.util.stream.Stream;
 
 public class Battleship {
+
+    Display display;
+    Input input;
+
     public static void main(String [] args) {
         System.out.println("Battleship works");
         Game game = new Game();
