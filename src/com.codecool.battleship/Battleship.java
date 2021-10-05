ee02912da7bc964164a4a540dd6b1eadf6532a71
predavies <iamosaretin@gmail.com>
2021-10-05 08:04:08 +0200

    refactor: [Battleship] make code cleaner

--- src/com.codecool.battleship/Battleship.java
+++ src/com.codecool.battleship/Battleship.java
@@ -16,11 +16,10 @@ public class Battleship {
     public static void main(String [] args) {
         System.out.println("Battleship works");
         Game game = new Game();
-        Arrays.stream(game.getBoard().getSquareList()).forEach(row ->
-        {Arrays.stream(row).forEach(square -> System.out.print(square.toString()));
+        Arrays.stream(game.getBoard().getSquareList()).forEach(row -> {
+            Arrays.stream(row).forEach(square -> System.out.print(square.toString()));
             System.out.println("\n");
         });
 
-//        System.out.println(Arrays.toString(game.getBoard()));
     }
 }
