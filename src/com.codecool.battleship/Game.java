7965186461ae2f83ff535dfc068d95d422783011
b-kozma <kozma.balint.1997@gmail.com>
2021-10-05 13:24:00 +0200

    refactor: put input methods to Input class

--- src/com.codecool.battleship/Game.java
+++ src/com.codecool.battleship/Game.java
@@ -15,27 +15,29 @@ public class Game {
 
     Display display = new Display();
 
+    Input input = new Input();
+
     // GAME STEPS METHODS
 
     public Board getBoard() {return board;}
 
     public void setBoardSize() {
-        int size = takeInputInteger("Give us a board size between 10 and 20!");
-        while (!isBetween(size, 10,20)) {
-            System.out.println("Between 10 and 20!");
-            size = takeInputInteger("Try again!");
+        int size = input.takeInteger("Give us a board size between 10 and 20!");
+        while (!input.isBetween(size, 10,20)) {
+            display.printMessages("Between 10 and 20!");
+            size = input.takeInteger("Try again!");
         }
         this.board = new Board(size);
     }
 
     public void setPlayers() {
         players[0] = new Player(
-                takeInputString("Tell us your name, Player 1!"),
+                input.takeString("Tell us your name, Player 1!"),
                 1,
                 "X");
 
         players[1] = new Player(
-                takeInputString("Tell us your name, Player 2!"),
+                input.takeString("Tell us your name, Player 2!"),
                 2,
                 "O"
         );
@@ -45,86 +47,10 @@ public class Game {
 
 
     public void placeShip(int player) {
-        Point input = takeInputCoordinates("Give coordinates!");
-        display.printMessages("TESTING: " + input.x + "-" + input.y + " is a good coordinate!");
+        Point point = input.takeCoordinates("Give coordinates!");
+        display.printMessages("TESTING: " + point.x + "-" + point.y + " is a good coordinate!");
         // TODO isValid(coordinates) —> while (!isValid(coord)) {input = takeInputCoord("Try again!");}
     }
-
-    // VALIDATIONS
-    public boolean isValid(Point input) {
-        if (input == null) {
-            display.printMessages("Input is null!");
-            return false;
-        } else if (isNegative(input)) {
-            display.printMessages("At least one input is negative!");
-            return false;
-        } else if (isOutOfBounds(input)) {
-            display.printMessages("At least one coordinate is out of bounds!");
-            return false;
-        } // More validations to be added.
-        display.printMessages("[TEST LOG] Valid input!");
-        return true;
-    }
-
-    public boolean validFormat(String coordinates) {
-        if (coordinates.length() > 3 || !coordinates.toLowerCase().matches("[a-z][0-9]+")) {
-            display.printMessages("Format your input in the following way:\n'B5' or 'B19'.");
-            return false;
-        }
-        return true;
-    }
-
-    public boolean isOutOfBounds(Point input) {
-        // Returns true if one of the x or y coordinates go beyond the board parameters
-        // Returns false if no problem is found
-        return ((input.x > board.getSquareList().length) || (input.y > board.getSquareList()[0].length));
-    }
-
-    public boolean isNegative(Point input) {
-        // Returns true if negative
-        // Returns false if positive
-        return (input.x < 0 || input.y < 0);
-    }
-
-
-    public boolean isBetween(int input, int min, int max) {
-        return (min <= input) && (input <= max);
-    }
-
-
-    // TAKE INPUT
-    public int takeInputInteger(String message) {
-        display.printMessages(message+"\n");
-        Scanner scanInteger = new Scanner(System.in);
-        int input = -1;
-        while (input == -1) {
-            try {
-                input = scanInteger.nextInt();
-            } catch (InputMismatchException e) {
-                display.printMessages("Mismatch error: Give Integer!");
-                scanInteger.next();
-            }
-        }
-        return input;
-    }
-
-    public String takeInputString(String message) {
-        display.printMessages(message+"\n");
-        Scanner scanInteger = new Scanner(System.in);
-        String result = scanInteger.nextLine();
-        return result;
-    }
-
-    public Point takeInputCoordinates(String message) {
-        String input = takeInputString(message);
-        while (!validFormat(input)) {
-            input = takeInputString("Try again!");
-        }
-        int x = (int) input.toLowerCase().charAt(0) - 97; // A —> 0
-        int y = Integer.parseInt(input.substring(1, input.length())) - 1; //
-        return new Point(x, y);
-    }
-
 
 
     // This method will naively produce subsequent references to squares based on orientation
--- src/com.codecool.battleship/Input.java
+++ src/com.codecool.battleship/Input.java
@@ -1,4 +1,86 @@
 package com.codecool.battleship;
 
+import java.awt.*;
+import java.util.InputMismatchException;
+import java.util.Scanner;
+import com.codecool.battleship.board.Display;
+import com.codecool.battleship.board.Board;
+
 public class Input {
+
+    Display display = new Display();
+
+    public boolean isValid(Board board, Point input) {
+        if (input == null) {
+            display.printMessages("Input is null!");
+            return false;
+        } else if (isNegative(input)) {
+            display.printMessages("At least one input is negative!");
+            return false;
+        } else if (isOutOfBounds(board, input)) {
+            display.printMessages("At least one coordinate is out of bounds!");
+            return false;
+        } // More validations to be added.
+        display.printMessages("[TEST LOG] Valid input!");
+        return true;
+    }
+
+    public boolean validFormat(String coordinates) {
+        if (coordinates.length() > 3 || !coordinates.toLowerCase().matches("[a-z][0-9]+")) {
+            display.printMessages("Format your input in the following way:\n'B5' or 'B19'.");
+            return false;
+        }
+        return true;
+    }
+
+    public boolean isOutOfBounds(Board board, Point input) {
+        // Returns true if one of the x or y coordinates go beyond the board parameters
+        // Returns false if no problem is found
+        return ((input.x > board.getSquareList().length) || (input.y > board.getSquareList()[0].length));
+    }
+
+    public boolean isNegative(Point input) {
+        // Returns true if negative
+        // Returns false if positive
+        return (input.x < 0 || input.y < 0);
+    }
+
+
+    public boolean isBetween(int input, int min, int max) {
+        return (min <= input) && (input <= max);
+    }
+
+
+    // TAKE INPUT
+    public int takeInteger(String message) {
+        display.printMessages(message+"\n");
+        Scanner scanInteger = new Scanner(System.in);
+        int input = -1;
+        while (input == -1) {
+            try {
+                input = scanInteger.nextInt();
+            } catch (InputMismatchException e) {
+                display.printMessages("Mismatch error: Give Integer!");
+                scanInteger.next();
+            }
+        }
+        return input;
+    }
+
+    public String takeString(String message) {
+        display.printMessages(message+"\n");
+        Scanner scanInteger = new Scanner(System.in);
+        String result = scanInteger.nextLine();
+        return result;
+    }
+
+    public Point takeCoordinates(String message) {
+        String input = takeString(message);
+        while (!validFormat(input)) {
+            input = takeString("Try again!");
+        }
+        int x = (int) input.toLowerCase().charAt(0) - 97; // A —> 0
+        int y = Integer.parseInt(input.substring(1, input.length())) - 1; //
+        return new Point(x, y);
+    }
 }
