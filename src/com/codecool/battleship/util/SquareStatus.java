79f3c4b24b16eb91be71727ab9a1e897d1c688ec
predavies <iamosaretin@gmail.com>
2021-10-06 10:06:26 +0200

    refactor: [SquareStatus] Change empty status unicode character color

--- src/com/codecool/battleship/util/SquareStatus.java
+++ src/com/codecool/battleship/util/SquareStatus.java
@@ -3,7 +3,7 @@ package com.codecool.battleship.util;
 public enum SquareStatus {
     /*TODO: change this to uniCode characters*/
 
-    EMPTY("."),
+    EMPTY("\033[1;34m" + "~" + "\033[0m"),
     SHIP("S"),
     HIT("H"),
     MISSED("M");
