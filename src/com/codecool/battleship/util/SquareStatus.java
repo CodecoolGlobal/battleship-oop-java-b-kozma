fe8981d3597affcb3bd8f9faa03f89c9ec4c578a
predavies <iamosaretin@gmail.com>
2021-10-06 10:28:42 +0200

    refactor: [SquareStatus] change square color

--- src/com/codecool/battleship/util/SquareStatus.java
+++ src/com/codecool/battleship/util/SquareStatus.java
@@ -4,9 +4,10 @@ public enum SquareStatus {
     /*TODO: change this to uniCode characters*/
 
     EMPTY("\033[1;34m" + "~" + "\033[0m"),
-    SHIP("\033[1;33m" + "S" + "\033[0m"),
-    HIT("\033[1;32m" + "H" + "\033[0m"),
-    MISSED("\033[1;31m" + "M" + "\033[0m");
+    SHIP(  "\033[0;37m" + "S" + "\033[0m"),
+    HIT("\033[0;33m" + "H" + "\033[0m"),
+    MISSED(  "\033[0;30m" + "M" + "\033[0m"),
+    SUNK(  "\033[0;31m" + "X" + "\033[0m");
 
     private String uniCodeCh;
     SquareStatus(String uniCodeCh) {
