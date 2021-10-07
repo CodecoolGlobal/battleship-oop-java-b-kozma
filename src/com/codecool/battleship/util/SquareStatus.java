e24746bcec3cde853a6740b3bf8c096ef6754a69
predavies <iamosaretin@gmail.com>
2021-10-07 11:34:25 +0200

    fix: [SquareStatus] change square statuses colors ship to green, and miss to white(gray)

--- src/com/codecool/battleship/util/SquareStatus.java
+++ src/com/codecool/battleship/util/SquareStatus.java
@@ -4,9 +4,9 @@ public enum SquareStatus {
     /*TODO: change this to uniCode characters*/
 
     EMPTY("\033[1;34m" + "~" + "\033[0m"),
-    SHIP(  "\033[0;37m" + "S" + "\033[0m"),
+    SHIP(   "\u001B[32m" + "S" + "\033[0m"),
     HIT("\033[0;33m" + "H" + "\033[0m"),
-    MISSED(  "\033[0;30m" + "M" + "\033[0m"),
+    MISSED(  "\033[0;37m" + "M" + "\033[0m"),
     SUNK(  "\033[0;31m" + "X" + "\033[0m");
 
     private String uniCodeCh;
